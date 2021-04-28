00241b8d3cf4add909ef0297d3475f41df512c5c
mohib <mohibumirza@gmail.com>
2021-04-27 17:53:20 -0700

    Model Importation working

--- ResourcePackImporter.py
+++ ResourcePackImporter.py
@@ -18,6 +18,8 @@ def init(namespace):
     modelsBlockPath = os.path.join(modelsPath, "block")
     texturesItemPath = os.path.join(texturesPath, "item")
     texturesBlockPath = os.path.join(texturesPath, "block")
+    texturesHatPath = os.path.join(texturesItemPath, "hats")
+    texturesHandheldPath = os.path.join(texturesItemPath, "handheld")
     if not os.path.exists(namespace):
         os.mkdir(namespace)
 
@@ -39,7 +41,13 @@ def init(namespace):
     if not os.path.exists(texturesBlockPath):
         os.mkdir(texturesBlockPath)
 
-def input():
+    if not os.path.exists(texturesHatPath):
+        os.mkdir(texturesHatPath)
+
+    if not os.path.exists(texturesHandheldPath):
+        os.mkdir(texturesHandheldPath)
+
+"""def input():
     namespace = "animecraft"
     modelFolder = "models/item/"
     print("Enter model's json file: ")
@@ -52,8 +60,13 @@ def input():
         hat = input()
         hat_str = "hats" if hat == 1 else "handheld"
         typepath = os.path.join(typepath, hat_str)
+
+    filePath = modelFolder + modelFile
+
+    return filePath, typePath"""
     
-    return modelFile, typePath
+    
+    
 
 def parseJSON(filePath):
     with open(filePath, "r") as file:
@@ -81,7 +94,7 @@ def parseJSON(filePath):
                 temp += char
 
         file.close()
-        return textureKeys, texturePaths
+        return textureKeys, texturePaths, modelData
 
 def repath(directory, paths):
     for i in len(paths):
@@ -93,36 +106,65 @@ namespace = "animecraft"
 init(namespace)
 modelFolder = "models/item/"
 modelFile = input("Enter model's json file: ")
+model_dump = os.path.join(namespace, "models")
+
+
 block = input("Is " + modelFile + " a block or item model? \n0 for item\n1 for block\n")
-typePath = "block" if block==1 else "item"
+typePath = "block" if block=="1" else "item"
+model_dump = os.path.join(model_dump, typePath, modelFile)
+
+
+if typePath == "item":
+    print("Is " + modelFile + " handheld or a hat? \n0 for handheld\n1 for hat\n")
+    hat = input()
+    hat_str = "hats" if hat=="1" else "handheld"
+    typePath = os.path.join(typePath, hat_str)
+
+
 filePath = modelFolder + modelFile
 print("Searching for ", filePath)
 if os.path.exists(filePath) is False:
     print("The file path is invalid!")
     sys.exit()
 
-textureKeys, texturePaths = parseJSON(filePath)
+textureKeys, texturePaths, modelData = parseJSON(filePath)
 print("Texture Keys: ", textureKeys)
 print("Texture Paths: ", texturePaths) # COPY FILE FROM EACH PATH INTO EITHER textures/block or textures/item
 
 texturesDirectory = os.path.join(namespace, "textures", typePath)
+print("TypePath: " + typePath)
 print(texturesDirectory)
 print(os.listdir(texturesDirectory))
-
+print(modelData)
 for i in range(len(texturePaths)):
     print(i)
     texture = texturePaths[i]
     texture = "textures/" + texture
-    print(os.path.basename(texture))
+    textureBasename = os.path.basename(texture)
     texture+= ".png"
+
     if(os.path.exists(texture)):
         copy(texture, texturesDirectory)
     else:
         print("ERROR: CANNOT FIND TEXTURE " + texture + " FROM ORIGINAL FILES!")
+
     texture+=".mcmeta"
+
     if(os.path.exists(texture)):
         copy(texture, texturesDirectory)
+
+    texturePaths[i] = os.path.join(typePath, textureBasename)
+    print(i, ": " +  texturePaths[i])
+    modelData["textures"][textureKeys[i]] = texturePaths[i].replace("\\", "/")
+    print(modelData["textures"][textureKeys[i]])
     
+print("Model_Dump: " + model_dump)
+
+with open(model_dump, "w") as write_file:
+    json.dump(modelData, write_file)
+
+
+
 
 print(os.listdir(texturesDirectory))
 
